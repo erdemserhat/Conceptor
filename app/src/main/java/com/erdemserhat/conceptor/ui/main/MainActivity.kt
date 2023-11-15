89a035fa49ab4c98a95a29047b177cf17632bdbd
Serhat <me.serhaterdem@gmail.com>
2023-11-15 22:47:34 +0300

    MVP Design

--- app/src/main/java/com/erdemserhat/conceptor/ui/main/MainActivity.kt
+++ app/src/main/java/com/erdemserhat/conceptor/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.erdemserhat.conceptor
+package com.erdemserhat.conceptor.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.erdemserhat.conceptor.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
